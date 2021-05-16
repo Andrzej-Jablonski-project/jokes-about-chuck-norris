91269eb124f0c4beb96fa4cdad7420b6b15ae2a4
Andrzej Jabłoński <andrzej@audioszum.pl>
2021-05-16 20:50:40 +0200

    clean code

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
@@ -21,17 +21,17 @@ export class DataService {
   }
 
   getJokeNerdy(): Observable<Joke[]> {
-     return this.http.get<JokeResult>(`${this.API_BASE_URL}/jokes/random?limitTo=[nerdy]`)
+    return this.http.get<JokeResult>(`${this.API_BASE_URL}/jokes/random?limitTo=[nerdy]`)
       .pipe(map(result => result.value));
   }
 
   getJokeExplicit(): Observable<Joke[]> {
     return this.http.get<JokeResult>(`${this.API_BASE_URL}/jokes/random?limitTo=[explicit]`)
-     .pipe(map(result => result.value));
+      .pipe(map(result => result.value));
  }
 
   getJokeFalseName(firstName: string, lastName: string): Observable<Joke[]> {
     return this.http.get<JokeResult>(`${this.API_BASE_URL}/jokes/random?firstName=${firstName}&lastName=${lastName}`)
-    .pipe(map(result => result.value));
+      .pipe(map(result => result.value));
   }
 }
