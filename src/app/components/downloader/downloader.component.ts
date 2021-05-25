d5b283579ebb5f9ad88c413d285ad562c9716823
Andrzej Jabłoński <andrzej@audioszum.pl>
2021-05-25 13:08:21 +0200

    feat: add download jokes, save to txt file

--- src/app/components/downloader/downloader.component.ts
+++ src/app/components/downloader/downloader.component.ts
@@ -1,4 +1,7 @@
+import { JokeService } from './../../services/joke.service';
 import { Component, OnInit } from '@angular/core';
+import { Joke } from 'src/app/models/joke';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-downloader',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DownloaderComponent implements OnInit {
 
-  constructor() { }
+  public counter = 0;
+  private jokes: string[] = [];
+
+  constructor(private dataService: JokeService) { }
 
   ngOnInit(): void {
+
   }
 
+  public getJokes(): void {
+      this.dataService.getJoke(`/${this.counter}`).subscribe( {
+        next: (data: Joke[]) =>  {
+          this.jokes = data.map( (joke: Joke, index: number) => `${index + 1}. ${joke.joke} \n` );
+        },
+        complete: () => { this.saveJokes(); },
+        error: () => {}
+      });
+  }
+
+  protected saveJokes(): void {
+    const blob = new Blob([`${this.jokes.join('')}`], {type: 'text/plain;charset=utf-8'});
+    FileSaver.saveAs(blob, 'jokes.txt');
+  }
+
+  public increaseCount(): void {
+      this.counter++;
+  }
+
+  public decreaseCount(): void {
+    if (this.counter > 0){
+      this.counter--;
+    }
+  }
 }
